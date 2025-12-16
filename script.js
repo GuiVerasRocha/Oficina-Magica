06afbd7e783f505471d4bf1a1df282402d0b90c0
GUILHERME VERAS DA ROCHA <57481277842@imaccommand014.local>
2025-12-16 11:56:12 -0300

    Site finalizado

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
+//Menu de Hamburguer
+let iconeBarras = document.getElementById("icone-barras")
+let iconeX = document.getElementById("icone-x")
+let body = document.getElementsByTagName("body")
+let sombra = document.getElementById("sombra")
 
-let boxBuscar = document.querySelector('.buscar-box');
-let lupa = document.querySelector('.lupa-buscar');
-let btnFechar = document.querySelector('.btn-fechar')
 
-lupa.addEventListener('click',()=>{
-    boxBuscar.classList.add('ativar')
-})
+function abreFechaMenu() {
+    //Se o menu está fechado
+    if (window.getComputedStyle(menu).right == "-420px") {
+        //Abrir menu
+        menu.style.right = "0"
+        
+        //Mostrar icone x
+        iconeX.style.display = "inline"
+        
+        //Esconder icone barras 
+        iconeBarras.style.display = "none"
+        
+        sombra.style.right = "-10vw"
+    } else {
+        //Fechar o menu
+        menu.style.right = "-420px"
+        
+        // Esconder icone X
+        iconeX.style.display = "none"
+        
+        // Mostrar icone barras
+        iconeBarras.style.display = "inline"
+        
+        sombra.style.right = "-101vw"
+    }
+}
 
-btnFechar.addEventListener('click',()=>{
-    boxBuscar.classList.remove('ativar')
-})
-
+onresize = () => {
+    if (window.getComputedStyle(menu).right == "-420px") {
+        // Mostrar icone X
+        iconeX.style.display = "inline"
+    }
+    else{
+        // Esconder icone barras
+        iconeBarras.style.display = "none"
+    }
+}
